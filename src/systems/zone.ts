9bd176f89042d3f0316af4c0277cfc25d14bf0f6
Chuanqi Sun <chuanqi.sun.dev@gmail.com>
2026-01-29 12:00:17 -0500

    format

--- src/systems/zone.ts
+++ src/systems/zone.ts
@@ -5,16 +5,7 @@ import type { System } from "../engine";
 /**
  * Check if two rectangles have any overlap.
  */
-function rectanglesOverlap(
-  x1: number,
-  y1: number,
-  w1: number,
-  h1: number,
-  x2: number,
-  y2: number,
-  w2: number,
-  h2: number
-): boolean {
+function rectanglesOverlap(x1: number, y1: number, w1: number, h1: number, x2: number, y2: number, w2: number, h2: number): boolean {
   return x1 < x2 + w2 && x1 + w1 > x2 && y1 < y2 + h2 && y1 + h1 > y2;
 }
 
